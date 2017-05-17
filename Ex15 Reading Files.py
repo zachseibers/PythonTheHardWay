7a4300ec769bfe908c9bb1d0b7607cf5c6f10df7
zachseibers <zachseibers@gmail.com>
2017-05-17 11:52:31 -0400

    Added comments to new commands

--- Ex15 Reading Files.py
+++ Ex15 Reading Files.py
@@ -4,14 +4,14 @@ from sys import argv
 
 script, filename =  argv
 
-txt = open(filename)
+txt = open(filename) #creates a file object from filename...allows the file's contents to be accessed later
 
 print("Here's your file %r:" % filename)
-print(txt.read())
+print(txt.read()) #reads the contents of txt, due to above that means we are reading the contents of filename
 
 print("Type the filename again:")
-file_again = input(">")
+file_again = input(">") 
 
-txt_again = open(file_again)
+txt_again = open(file_again) #same as previous, just different names. Note that file name can be stored as variable in python code
 
-print(txt_again.read())
+print(txt_again.read()) #same as previous use.
