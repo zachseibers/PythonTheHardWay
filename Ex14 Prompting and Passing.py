e2bbc0198117fd83c3e4b24a852180a5019447e1
zachseibers <zachseibers@gmail.com>
2017-05-17 11:12:46 -0400

    Added comments to Ex14 to explain new lines.

--- Ex14 Prompting and Passing.py
+++ Ex14 Prompting and Passing.py
@@ -3,9 +3,10 @@
 from sys import argv
 
 script, user_name = argv
-prompt = '>'
+prompt = '>' #This is the string that appears before the space inwhich the user would respond
 
-print("Hi %s, I'm the %s script." %(user_name, script))
+
+print("Hi %s, I'm the %s script." %(user_name, script))  #%s is the variable placeholder for cript inputs
 print("I'd like to ask you a few questions.")
 print("Do you like me %s?" % user_name)
 likes = input(prompt)
@@ -20,4 +21,4 @@ print("""
 Alright, so you said %r about liking me. 
 You live in %r. Not sure where that is. 
 And you have a %r computer. Nice. 
-""" %(likes, lives, computer))
+""" %(likes, lives, computer))  # %r is the variable placeholder for an output.
