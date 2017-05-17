f78b374068f6ecd973a04a8da0d9b3efda77d47a
zachseibers <zachseibers@gmail.com>
2017-05-17 10:55:11 -0400

    Initial commits of Ex12 & Ex13

---
+++ Ex12 Prompting People.py
@@ -0,0 +1,7 @@
+#Ex12: Prompting People
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print("So, you're %r old, %r tall, and %r heavy." %(age, height, weight))
