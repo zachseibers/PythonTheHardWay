1324aefc336783af06facfb023c6f8a05f936b39
zachseibers <zachseibers@gmail.com>
2017-05-17 10:27:11 -0400

    Initial commit of Ex11

---
+++ Ex11 Asking Questions.py
@@ -0,0 +1,12 @@
+#Ex11 Asking Questions
+
+print("How old are you?")
+age = input()
+
+print("How tall are you?",)
+height = input()
+
+print("How much do you weigh?",)
+weight = input()
+
+print("So, you're %r old, %r tall and %r heavy." %(age, height, weight))
