b7ce423eb75b57afa0b4c45b2aa8cf65e65e3e87
zachseibers <zachseibers@gmail.com>
2017-05-18 13:04:52 -0400

    Combined lines 11 and 12 as per challenge

--- Ex17 More FIles.py
+++ Ex17 More FIles.py
@@ -3,7 +3,7 @@
 from sys import argv
 from os.path import exists
 
-script, from_file, to_file = argv
+script, from_file, to_file = argv     #stores command line inputs to respective variables according to order
 
 print("Copying from %s to %s" % (from_file, to_file))
 
@@ -11,9 +11,12 @@ print("Copying from %s to %s" % (from_file, to_file))
 in_file = open(from_file)
 indata = in_file.read()
 
+#This is how it would be done
+#indata = open(from_file).read() 
+
 print("The input file is %d bytes long" %len(indata))
 
-print("Does the output file exist? %r" % exists(to_file))
+print("Does the output file exist? %r" % exists(to_file)) #Exists tests to see if the argument file already exists
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
 input()
 
