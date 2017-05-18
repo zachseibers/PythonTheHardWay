45541aef10149ae99ac75cbd95292599028dafad
zachseibers <zachseibers@gmail.com>
2017-05-18 09:00:06 -0400

    Added comments to explain new lines

--- Ex16 Reading and Writing Files.py
+++ Ex16 Reading and Writing Files.py
@@ -11,10 +11,10 @@ print("If you do want that, hit RETURN.")
 input("?")
 
 print("Opening the file...")
-target = open(filename, 'w')
-
+target = open(filename, 'w') #opens the target file so that the program can use it, notice it is stored as a variable
+# In the above line, w means open the file in write mode
 print("Truncating the file. Goodbye!")
-target.truncate()
+target.truncate() #Empties the contents of the file, not to be used carelessly
 
 print("Now I'm going to ask you for three lines.")
 
@@ -24,7 +24,7 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the new file.")
 
-target.write(line1)
+target.write(line1) #Writes variable or string to file, note that in this case target is the variable of the open file
 target.write("\n")
 target.write(line2)
 target.write("\n")
@@ -32,4 +32,4 @@ target.write(line3)
 target.write("\n")
 
 print("And finally, we close it")
-target.close()
+target.close()  #Closes the file, similar to close & save in a text editor
