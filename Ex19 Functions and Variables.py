1f43195fafe174f3168b1783da5fb3e65bd21dfb
zachseibers <zachseibers@gmail.com>
2017-05-18 15:25:43 -0400

    Fixed typo in cheese_and_crackers

--- Ex19 Functions and Variables.py
+++ Ex19 Functions and Variables.py
@@ -1,6 +1,6 @@
 #Ex19 Functions and Variables
 
-def chees_and_crackers(cheese_count, boxes_of_crackers):
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print("You have %d cheeses!" %cheese_count)
     print("You have %d boxes of crackers!" %boxes_of_crackers)
     print("Man that's enough for a party!")
@@ -16,8 +16,8 @@ amount_of_crackers = 50
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 print("We can even do math inside too:")
-chees_and_crackers(10 + 20, 5 + 6)
+cheese_and_crackers(10 + 20, 5 + 6)
 
 print("And we can combine the two, variables and math:")
-chees_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
 
