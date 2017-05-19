ad465be6b97118864e51c4f86be173b6b3802d96
zachseibers <zachseibers@gmail.com>
2017-05-19 11:22:10 -0400

    Initial Commit Ex23a Initial Research

--- Ex23a Initial Research.py
+++ Ex23a Initial Research.py
@@ -10,13 +10,13 @@ def main(language_file, encoding, errors):
         print_line(line, encoding, errors)
         return main(language_file, encoding, errors)
 
-def print_line(line, encoding, errors)
+def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
     cooked_string = raw_bytes.decode(encoding, errors=errors)
 
     print(raw_bytes, "<===>", cooked_string)
 
-languages = open("languages.txt", encodings="utf-8")
+languages = open("languages.txt", encoding="utf-8")
 
 main(languages, encoding, error)
