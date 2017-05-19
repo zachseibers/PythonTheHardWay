33c0f8d2eddba8be1fc97caf83fe0ac160a10001
zachseibers <zachseibers@gmail.com>
2017-05-19 11:46:55 -0400

    Added comments for explanation

--- Ex24 More Practice.py
+++ Ex24 More Practice.py
@@ -27,12 +27,12 @@ def secret_formula(started):
     return jelly_beans, jars, crates
 
 start_point = 10000
-beans, jars, crates = secret_formula(start_point)
+beans, jars, crates = secret_formula(start_point) # Variables beans, jars, and crates get defined by the function:secret formula
 
 # remember that this is another way to format a string
 print("With a starting point of: {}".format(start_point))
 # it's just like with an f"" string
-print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
+print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.") # The name of each placeholder (ex: {beans}) does not seem to matter
 
 start_point = start_point / 10
 
