f371abc5ae12e0153a6e7411b1f910e4886f880b
zachseibers <zachseibers@gmail.com>
2017-05-22 14:13:32 -0400

    Added comments to explain new concepts

--- Ex33 While Loops.py
+++ Ex33 While Loops.py
@@ -1,18 +1,17 @@
 #Ex33 While Loops
 
 i = 0
-numbers = []
+numbers = [] #define empty list
 
 while i < 6:
     print(f"At the top i is {i}")
     numbers.append(i)
     
-    i = i + 1 
-    print("Numbers now: ", numbers)
+    i += 1 #changed from i = i + 1
+    print("Numbers now: ", numbers) # prints the contents of numbers
     print(f"At the bottom i is {i}")
 
 print("The numbers: ")
 
-for num in numbers:
+for num in numbers: #looping through the list  titled "numbers", note that num is a new 'counter' variable
     print(num)
-    
