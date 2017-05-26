4fd72f28333cda1cfffef2e7fee06e53cb387ab7
zachseibers <zachseibers@gmail.com>
2017-05-26 09:43:15 -0400

    Updated Comments Ex35

--- Ex35 Branches and Functions.py
+++ Ex35 Branches and Functions.py
@@ -6,7 +6,7 @@ def gold_room():                 #Note that since these are functions, the order
     print("This room is full of gold. How much do you take?")
 
     choice = input(">")
-    if "0" in choice or "1" in choice:
+    if "0" in choice or "1" in choice: #Dayton is awesome
         how_much = int(choice)
     else:
         dead("Man, learn to type a number.")
