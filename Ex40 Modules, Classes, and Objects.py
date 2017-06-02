31e0e41ddd3eb0f680380560a9c20bda113bf6ec
zachseibers <zachseibers@gmail.com>
2017-06-02 13:35:07 -0400

    Ex40 Initial commit - Modules, Classes, and Objects

---
+++ Ex40 Modules, Classes, and Objects.py
@@ -0,0 +1,25 @@
+#Ex40 Modules, Classes, and Objects
+
+#mystuff = {'apple':"I AM APPLES!"}     #First example in online tutorial
+#print(mystuff['apple'])
+
+class Song(object):
+
+    def__init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+    
+happy_bday = Song(["Happy birthday to you",
+                    "I don't want to get sued",
+                    "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around tha family", 
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
