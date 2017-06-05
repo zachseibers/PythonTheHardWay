716d7ce995f826f490f7eb51611e8f70a6032811
zachseibers <zachseibers@gmail.com>
2017-06-05 09:25:52 -0400

    Tried to answer questions, no answer key

--- Ex42 Objects and Classes Test.py
+++ Ex42 Objects and Classes Test.py
@@ -4,21 +4,21 @@
 class Animal(object):
     pass
 
-## ??
+## Animal has-a class dog
 class Dog(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## name is an object in claa Dog
         self.name = name
 
-## ??
+## Animal has a class cat
 class Cat(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## self.name is-a object in the cat class
         self.name = name
 
-## ??
+## Person is a class
 class Person(object):
 
     def __init__(self, name):
@@ -28,41 +28,41 @@ class Person(object):
         ## Person has-a pet of some kind
         self.pet = None
 
-## ??
+## Fish is-a object
 class Fish(object):
     pass
 
-## ??
+## Salmon is-a object
 class Salmon(Fish):
     pass
 
-## ??
+## Halibut is-a-object
 class Halibut(Fish):
     pass
 
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## satan is a Cat
 satan = Cat("Satan")
 
-## ??
+## Mary is a person
 mary = Person("Mary")
 
-## ??
+## satan is-a instance of mary's pet
 mary.pet = satan
 
-## ??
+## Frank is a object within the Employees class
 frank = Employees("Frank", 120000)
 
-## ??
+## Rover is a instance of Frank's pet
 frank.pet = rover
 
-## ??
+## flipper is an instance of fish object
 flipper = Fish()
 
-## ??
+## flipper is an instance of Salmon object
 crouse = Salmon()
 
-## ??
+## flipper is an instance of Halibut object
 harry = Halibut()
